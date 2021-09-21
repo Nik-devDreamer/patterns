725c16dec802dccf9bcdbc4352d4e9400afa2a52
Nikita <80746832+Nikita-Programmer@users.noreply.github.com>
2021-09-21 17:33:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WD.Publisher;
+using WD.Subscriber;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherData weatherdata = new WeatherData();
+            CurrentDisplay display = new CurrentDisplay();
+            StatisticsDisplay display2 = new StatisticsDisplay();
+            weatherdata.AddSubscriber(display);
+            weatherdata.AddSubscriber(display2);
+            weatherdata.NotifySubscriber();
+            Console.ReadKey();
+        }
+    }
+}
