7e3dd19e675d0c87db99b56883737521c534a791
Nikita <80746832+Nikita-Programmer@users.noreply.github.com>
2021-11-29 16:55:38 +0200

    Add files via upload

---
+++ Program10/PancakeHouseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program10
+{
+    public class PancakeHouseMenu : IMenu
+    {
+        List<MenuItem> menuItems;
+        public PancakeHouseMenu()
+        {
+            menuItems = new List<MenuItem>();
+
+            AddItem("K&B's Pancake Breakfast", "Pancakes with scramled eggs, and toast", true, 2.99);
+            AddItem("Regular Pancake Breakfast", "Pancakes with fried eggs, sausage", false, 2.99);
+            AddItem("Blueberry pancakes", "Pancakes made with fresh blueberries", true, 3.49);
+            AddItem("Waffles", "Waffles, with your choice of blueberries or strawberries", true, 3.59);
+        }
+
+        public void AddItem(string name, string description, bool vegetarian, double price)
+        {
+            MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+            menuItems.Add(menuItem);
+        }
+        public IIterator<MenuItem> CreateIterator()
+        {
+            return new PancakeHouseMenuIterator(menuItems);
+        }
+    }
+}
